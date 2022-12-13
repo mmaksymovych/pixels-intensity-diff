1706922c968ed3df4a4cdf41b02712615c8b34fc
maxym <maxym@getbalance.com>
2022-12-14 01:18:23 +0200

    images processing

--- src/App.js
+++ src/App.js
@@ -13,51 +13,65 @@ import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
 const drawerWidth = 240;
 const navItems = ["Home", "About", "Contact"];
 
-const w = 400;
-const h = 200;
+const w = 1000;
+const h = 800;
 
-const load = (path) => {
-  return new Promise((resolve, reject) => {
-    const img = new Image();
-    img.src = path;
-    img.onload = () => {
-      resolve(img);
-    };
-  });
-};
+const Input = ({ id }) => (
+  <Box display="flex" flexDirection="column">
+    <input
+      type="file"
+      name="myImage"
+      onChange={async (event) => {
+        var file = event.target.files[0];
 
-function draw(img) {
-  const canvas = document.getElementById("canvas1");
-  canvas.width = w;
-  canvas.height = h;
-  document.body.appendChild(canvas);
+        var reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend = function (e) {
+          console.log("res", e.target.result);
 
-  const ctx = canvas.getContext("2d");
-  ctx.filter = "blur(4px)";
-  ctx.drawImage(img, 0, 0, w, h);
-  return ctx.getImageData(0, 0, w, h);
-}
+          var image = new Image();
+          image.src = e.target.result;
+          image.onload = function (ev) {
+            var canvas = document.getElementById(id);
+            canvas.width = w;
 
-async function loadImages() {
-  const img1 = await load(
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png"
-  );
-  const data1 = draw(img1);
-}
+            const percent = canvas.width / image.width;
+
+            console.log("p", percent);
+
+            // const height = image.height * percent;
+            const height = h;
+
+            canvas.height = height;
+            var ctx = canvas.getContext("2d");
+
+            ctx.drawImage(image, 0, 0, w, h);
+          };
+        };
+      }}
+    />
+    <canvas id={id} />
+  </Box>
+);
 
 function DrawerAppBar(props) {
   const { window } = props;
   const [mobileOpen, setMobileOpen] = React.useState(false);
-
-  React.useEffect(() => {
-    console.log("draw images");
-
-    loadImages();
-  }, []);
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
@@ -133,38 +147,71 @@ function DrawerAppBar(props) {
           {drawer}
         </Drawer>
       </Box>
-      <Box component="main" sx={{ p: 3 }}>
-        <Toolbar />
 
-        <input
-          type="file"
-          name="myImage"
-          onChange={async (event) => {
-            var file = event.target.files[0];
-
-            console.log(file);
-
-            var reader = new FileReader();
-            reader.readAsDataURL(file);
-            reader.onloadend = function (e) {
-              console.log("res", e.target.result);
-
-              var image = new Image();
-              image.src = e.target.result;
-              image.onload = function (ev) {
-                var canvas = document.getElementById("canvas1");
-                canvas.width = image.width;
-                canvas.height = image.height;
-                var ctx = canvas.getContext("2d");
-                ctx.drawImage(image, 0, 0);
-              };
-            };
-          }}
-        />
-        <canvas id="canvas1" />
+      <Box sx={{ width: "100%" }} mt={10} px={5}>
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid item xs={6}>
+            <Input id="1" />
+          </Grid>
+          <Grid item xs={6}>
+            <Input id="2" />
+          </Grid>
+          <Grid item xs={12}>
+            <Item>
+              <Box display="flex" flexDirection="column">
+                <Button variant="contained" onClick={processImages}>
+                  Process
+                </Button>
+                <canvas id="result"></canvas>
+              </Box>
+            </Item>
+          </Grid>
+        </Grid>
       </Box>
     </Box>
   );
+}
+
+function processImages() {
+  const canvas1 = document.getElementById("1");
+  const canvas2 = document.getElementById("2");
+
+  const ctx1 = canvas1.getContext("2d");
+  const ctx2 = canvas2.getContext("2d");
+
+  const data1 = ctx1.getImageData(0, 0, w, h);
+  const data2 = ctx2.getImageData(0, 0, w, h);
+
+  const canvas = document.getElementById("result");
+  canvas.width = w;
+  canvas.height = h;
+
+  const ctx = canvas.getContext("2d");
+
+  ctx.rect(0, 0, w, h);
+  ctx.fill();
+  const data3 = ctx.getImageData(0, 0, w, h);
+
+  for (var i = 0; i < data1.data.length; i += 4) {
+    var ir = data1.data[i];
+    var ig = data1.data[i + 1];
+    var ib = data1.data[i + 2];
+
+    var fr = data2.data[i];
+    var fg = data2.data[i + 1];
+    var fb = data2.data[i + 2];
+
+    const dr = Math.abs(ir - fr) > 10 ? fr : 0;
+    const dg = Math.abs(ig - fg) > 10 ? fg : 0;
+    const db = Math.abs(ib - fb) > 10 ? fb : 0;
+
+    const pxchanged = dr > 0 && dg > 0 && db > 0;
+    data3.data[i] = pxchanged ? 255 : 0;
+    data3.data[i + 1] = pxchanged ? 0 : 0;
+    data3.data[i + 2] = pxchanged ? 0 : 0;
+  }
+
+  ctx.putImageData(data3, 0, 0);
 }
 
 DrawerAppBar.propTypes = {
