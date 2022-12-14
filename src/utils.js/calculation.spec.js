867d1503fc4d8d8438a8b2ba2b8677ef862e3245
maxym <maxym@getbalance.com>
2022-12-14 23:29:48 +0200

    deviation calculation

---
+++ src/utils.js/calculation.spec.js
@@ -0,0 +1,7 @@
+describe('calculation', () => {
+  test('2+2', () => {
+    expect(2+2).toBe(4)
+  })
+
+  test('')
+})
--- src/utils.js/processing.js
+++ src/utils.js/processing.js
@@ -1,4 +1,5 @@
 import { width, height } from "../constants";
+var stats = require("stats-lite");
 
 export const processImages = () => {
   const canvas1 = document.getElementById("1");
@@ -20,6 +21,8 @@ export const processImages = () => {
   ctx.fill();
   const data3 = ctx.getImageData(0, 0, width, height);
 
+  const diffs = [];
+
   for (var i = 0; i < data1.data.length; i += 4) {
     var ir = data1.data[i];
     var ig = data1.data[i + 1];
@@ -29,15 +32,23 @@ export const processImages = () => {
     var fg = data2.data[i + 1];
     var fb = data2.data[i + 2];
 
-    const dr = Math.abs(ir - fr) > 10 ? fr : 0;
-    const dg = Math.abs(ig - fg) > 10 ? fg : 0;
-    const db = Math.abs(ib - fb) > 10 ? fb : 0;
+    const diffr = Math.abs(ir - fr);
+    const diffg = Math.abs(ig - fg);
+    const diffb = Math.abs(ib - fb);
+
+    diffs.push(diffr, diffg, diffb);
+
+    const dr = diffr > 10 ? fr : 0;
+    const dg = diffg > 10 ? fg : 0;
+    const db = diffb > 10 ? fb : 0;
 
     const pxchanged = dr > 0 && dg > 0 && db > 0;
     data3.data[i] = pxchanged ? 255 : 0;
     data3.data[i + 1] = pxchanged ? 0 : 0;
     data3.data[i + 2] = pxchanged ? 0 : 0;
   }
+
+  console.log("standard deviation: %s", stats.stdev(diffs));
 
   ctx.putImageData(data3, 0, 0);
 };
